2ee515b517702c9c8737e6a28281e8ae2693c574
andlaz <andras.szerdahelyi@gmail.com>
2015-07-30 12:18:37 +0200

    Fixed argument line re-writing in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,11 +7,13 @@
 add_cassandra_options () {
 	local opts=""
 	# if --seed is not set, set it to the alias host
-	if [ $SEED_NAME ] && [ ! "$*" == *"--seeds"* ]; then opts="$opts --seeds seed"; fi
+	
+	if [ ! $SEED_NAME ] && [[ $* != *"--seeds"* ]]; then opts="$opts --seeds "$(ip route get 255.255.255.255 | grep -Po '(?<=src )(\d{1,3}.){4}');
+	elif [ $SEED_NAME ] && [[ $* != *"--seeds"* ]]; then opts="$opts --seeds seed"; fi
 	# if --listen-address is not set, set it to $HOSTNAME
-	if [ $HOSTNAME ] && [ ! "$*" == *"--listen-address"* ]; then opts="$opts --listen-address $HOSTNAME"; fi
+	if [ $HOSTNAME ] && [[ $* != *"--listen-address"* ]]; then opts="$opts --listen-address $HOSTNAME"; fi
 	# if --rpc-address is not set, set it to $HOSTNAME
-	if [ $HOSTNAME ] && [ ! "$*" == *"--rpc-address"* ]; then opts="$opts --rpc-address $HOSTNAME"; fi
+	if [ $HOSTNAME ] && [[ $* != *"--rpc-address"* ]]; then opts="$opts --rpc-address $HOSTNAME"; fi
 	
 	echo $opts
 }
@@ -33,15 +35,15 @@ add_nodemanager_options () {
 }
 
 case $1 in
-	all) ruby /root/configure.rb $* `add_cassandra_options` `add_datanode_options` `add_nodemanager_options` && supervisord -c /etc/supervisord.conf ;;
-	cassandra) ruby /root/configure.rb $* `add_cassandra_options` && supervisord -c /etc/supervisord.conf ;;
-	datanode) ruby /root/configure.rb $* `add_datanode_options` && supervisord -c /etc/supervisord.conf ;;
-	nodemanager) ruby /root/configure.rb $* `add_nodemanager_options` && supervisord -c /etc/supervisord.conf ;;
+	all) ruby /root/configure.rb $* `add_cassandra_options $*` `add_datanode_options $*` `add_nodemanager_options $*` && supervisord -c /etc/supervisord.conf ;;
+	cassandra) ruby /root/configure.rb $* `add_cassandra_options $*` && supervisord -c /etc/supervisord.conf ;;
+	datanode) ruby /root/configure.rb $* `add_datanode_options $*` && supervisord -c /etc/supervisord.conf ;;
+	nodemanager) ruby /root/configure.rb $* `add_nodemanager_options $*` && supervisord -c /etc/supervisord.conf ;;
 	help) cat << EOM
 The image's entry point script will populate the following -parameters from linked
 containers and Docker environment variables:
 EOM
-	echo -e "\ncassandra\t:" `add_cassandra_options` "\ndatanode\t:" `add_datanode_options` "\nnodemanager\t:" `add_nodemanager_options`
+	echo -e "\ncassandra\t:" `add_cassandra_options $*` "\ndatanode\t:" `add_datanode_options $*` "\nnodemanager\t:" `add_nodemanager_options $*`
 	echo -e "\n( Cassandra seed, HDFS NameNode and YARN ResourceManager countainers need to be linked under the aliases "seed", "namenode" and "resourcemanager" for this to happen. )\n"
 	ruby /root/configure.rb $* ;;
 	*) ruby /root/configure.rb $*
