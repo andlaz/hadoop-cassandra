a0b8d72ca93fd01c56d559cecac62015bf6d0d9d
andlaz <andras.szerdahelyi@gmail.com>
2015-08-23 21:01:17 +0200

    Allow configure.rb parameters to come from an environment variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,13 @@
 # to configure.rb parameters
 # ----------------------------------------------
 
+# Allow parameters to configure.rb to come from CONFIGURE_PARAMS as opposed to stdin
+# This is to support parameterizing via Compose in particular. The two lines below are equivalent in effect:
+# docker run -e "CONFIGURE_PARAMS=cassandra --dc daas-1 --calculate-tokens 1:1" -ti --name c1 --rm andlaz/hadoop-cassandra
+# docker run -ti --name c1 --rm andlaz/hadoop-cassandra cassandra --dc daas-1 --calculate-tokens 1:1
+P=${CONFIGURE_PARAMS:-$(echo $*)}
+set -- $P
+
 add_cassandra_options () {
 	local opts=""
 	# if --seed is not set, set it to the alias host
